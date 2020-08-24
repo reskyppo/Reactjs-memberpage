341961a7a2b5f6e38ed1b2d0e8db299df07d2720
Reskyppo <reskybeen@gmail.com>
2020-08-25 00:05:49 +0700

    make loginform component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 /* eslint-disable jsx-a11y/heading-has-content */
-import React from 'react';
-import 'assets/css/style.css';
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+import GuestRoute from "components/Routes/GuestRoute";
+
+import Login from "pages/Login";
+import NotFound from "pages/404";
+
+import "assets/css/style.css";
 
 function App() {
+  const history = createBrowserHistory({ basename: process.env.PUBLIC_URL });
   return (
-    <div className="container mx-auto">
-      <h1 className="text-6xl">Java</h1>
-    </div>
+    <>
+      <Router history={history}>
+        <Switch>
+          <GuestRoute path="/login" component={Login}></GuestRoute>
+          <Route path="*" component={NotFound} ></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
