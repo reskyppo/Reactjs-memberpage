87189a7334821cdcb7743a4bac1aeaf7b2e9ea9e
Reskyppo <reskybeen@gmail.com>
2020-08-24 20:48:08 +0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/* eslint-disable jsx-a11y/heading-has-content */
+import React from 'react';
+import 'assets/css/style.css';
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-6xl">Java</h1>
+    </div>
+  );
+}
+
+export default App;
