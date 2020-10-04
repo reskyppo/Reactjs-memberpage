569c1e9f30a367890ff98f3d9f45157ca567c64f
Reskyppo <reskybeen@gmail.com>
2020-10-04 22:47:53 +0700

    handle jwt

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/heading-has-content */
-import React from "react";
+import React, { useEffect } from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
 
@@ -14,10 +14,29 @@ import Unauthenticated from "pages/401";
 
 import MyClass from "pages/MyClass";
 
+import { setAuthorizationHeader } from "configs/axios";
+import { useDispatch } from "react-redux";
+import { populateProfile } from "store/actions/users";
+import users from "constans/api/users";
+
 import "assets/css/style.css";
 
 function App() {
   const history = createBrowserHistory({ basename: process.env.PUBLIC_URL });
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    let session = null;
+    if (localStorage.getItem("BWAMICRO:token")) {
+      session = JSON.parse(localStorage.getItem("BWAMICRO:token"));
+      setAuthorizationHeader(session.token);
+      users.details().then((detail) => {
+        dispatch(populateProfile(detail.data));
+      });
+    }
+  }, [dispatch]);
+
   return (
     <>
       <Router history={history}>
